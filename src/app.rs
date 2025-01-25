6ac80380b7f01c6da32ca89802e71a2b2d1f8b58
jefferson <jefferson.science@gmail.com>
2025-01-25 20:31:27 -0300

    working

--- src/app.rs
+++ src/app.rs
@@ -1,20 +1,26 @@
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
-#[serde(default)] // if we add new fields, give them default values when deserializing old state
+#[serde(default)] // if we add new fields, give them default idades when deserializing old state
 pub struct TemplateApp {
     // Example stuff:
-    label: String,
-
-    #[serde(skip)] // This how you opt-out of serialization of a field
-    value: f32,
+    visible: bool,
+    nome: String,
+    massa: f32,
+    idade: i32,
+    apresentacao_selecionada: String,
+    apresentacoes: Vec<String>, //#[serde(skip)] // This how you opt-out of serialization of a field
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
             // Example stuff:
-            label: "Teste!".to_owned(),
-            value: 2.9,
+            visible: true,
+            nome: "Nome".to_owned(),
+            massa: 2.5,
+            idade: 2,
+            apresentacoes: vec!["mg/dL".to_owned(), "mg/Kg/h".to_owned()],
+            apresentacao_selecionada: "mg".to_owned(),
         }
     }
 }
@@ -46,18 +52,30 @@ impl eframe::App for TemplateApp {
         // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
 
-        egui::Window::new("Janela Flutuante")
+        egui::Window::new("Identificação")
             .default_pos((100.0, 100.0)) // Posição inicial
             .default_width(80.0)
             .default_height(80.0)
             .resizable(true) // Permite redimensionar
             .show(ctx, |ui| {
-                ui.label("Conteúdo da janela flutuante!");
+                ui.label("Ficha pessoal");
                 ui.horizontal(|ui| {
                     ui.label("Nome do paciente: ");
-                    ui.text_edit_singleline(&mut self.label);
+                    ui.text_edit_singleline(&mut self.nome);
                 });
             });
+
+        if self.visible {
+            let _ = egui::Area::new("popup".into())
+                .movable(true)
+                .show(ctx, |ui| {
+                    ui.heading("Popup Flutuante");
+                    ui.label("Este é um exemplo de área flutuante.");
+                    if ui.button("Fechar").clicked() {
+                        self.visible = false;
+                    }
+                });
+        };
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             // The top panel is often a good place for a menu bar:
@@ -84,21 +102,46 @@ impl eframe::App for TemplateApp {
 
             ui.horizontal(|ui| {
                 ui.label("Nome do paciente: ");
-                ui.text_edit_singleline(&mut self.label);
+                ui.text_edit_singleline(&mut self.nome);
             });
 
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("Idade"));
-            if ui.button("Incrementar").clicked() {
-                self.value += 1.0;
-            }
+            ui.add(egui::Slider::new(&mut self.idade, 0..=100).text("Idade"));
+            if ui.button("Incrementar 1 ano").clicked() {
+                self.idade += 1;
+            };
 
-            ui.add(egui::Slider::new(&mut self.value, 1.0..=10.0).text("Massa"));
+            ui.add(egui::Slider::new(&mut self.massa, 1.0..=200.0).text("Massa"));
             if ui.button("Incrementar 100g").clicked() {
-                self.value += 0.1;
-            }
+                self.massa += 0.1;
+            };
             if ui.button("Incrementar 1kg").clicked() {
-                self.value += 1.0;
-            }
+                self.massa += 1.0;
+            };
+
+            egui::ComboBox::from_label("Escolha uma apresentação")
+                .selected_text(&self.apresentacao_selecionada)
+                // Adiciona as opções ao menu
+                .show_ui(ui, |ui: &mut egui::Ui| {
+                    ui.selectable_value(
+                        &mut self.apresentacao_selecionada,
+                        "mg/dL".to_string(),
+                        "mg/dL",
+                    );
+                    ui.selectable_value(
+                        &mut self.apresentacao_selecionada,
+                        "mg/Kg".to_string(),
+                        "mg/Kg",
+                    );
+                    ui.selectable_value(&mut self.apresentacao_selecionada, "mg".to_string(), "mg");
+                });
+            // for item in &self.apresentacoes {
+            //             ui.selectable_value(
+            //                 &mut self.apresentacao_selecionada, // Referência mutável ao item selecionado
+            //                 item.clone(),            // Valor a ser definido se selecionado
+            //                 item,                    // Texto exibido
+            //             );
+            //         }
+
             ui.separator();
 
             ui.add(egui::github_link_file!(
@@ -121,6 +164,5 @@ fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
             "Um projeto de Jefferson T.",
             "https://jeffersontorres.com.br/",
         );
-        ui.label(".");
     });
 }
